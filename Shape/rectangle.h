e606bb6672bcac67239ae6fd3db6e0e894711161
Dominique Septembre <dominique.septembre@gmail.com>
2015-03-10 15:14:17 -0400

    Rectangle fonctionnels

--- Shape/rectangle.h
+++ Shape/rectangle.h
@@ -3,10 +3,17 @@
 
 namespace ShapeLibrary
 {
-	class Rectangle
+	class Rectangle : public Shape
 	{
 	public:
+		Rectangle(IWindowAPI& _windowAPI);
+		void setPosition(Point & _point);
+		void setWidth(const int _width);
+		void setHeight(const int _height);
+		void draw() const override;
 
-	
+	private:
+		unsigned int width;
+		unsigned int height;
 	};
 }
--- Shape/shape.cpp
+++ Shape/shape.cpp
@@ -2,4 +2,53 @@
 
 using namespace ShapeLibrary;
 
+Shape::Shape(IWindowAPI& _windowAPI)
+{
+	this->windowAPI = &_windowAPI;
+}
 
+void Shape::draw() const
+{
+	windowAPI->setDrawingColor(lineColor);
+}
+
+void Shape::setLineColor(const Color& _color)
+{
+	lineColor = _color;
+}
+
+const Color& Shape::getLineColor() const
+{
+	return lineColor;
+}
+
+void Shape::setFillColor(const Color& _color)
+{
+	fillColor = _color;
+}
+
+const Color& Shape::getFillColor() const
+{
+	return fillColor;
+}
+
+const Point& Shape::getPoint(const unsigned _index) const
+{
+	if (_index > getNumberOfPoints())
+		throw invalid_argument("L'index est plus grand que le nombre de points.");
+	return point[_index];
+}
+
+unsigned int Shape::getNumberOfPoints() const
+{
+	return point.size();
+}
+
+void Shape::add(const Point& _point)
+{
+	point.push_back(_point);
+}
+
+Shape::~Shape()
+{
+}
