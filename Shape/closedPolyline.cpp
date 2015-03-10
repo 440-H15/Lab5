67f766f9efd271425e27968ca8378edd132e288a
Dominique Septembre <dominique.septembre@gmail.com>
2015-03-10 15:44:57 -0400

    closedPolyLine

--- Shape/closedPolyline.cpp
+++ Shape/closedPolyline.cpp
@@ -2,3 +2,22 @@
 
 using namespace ShapeLibrary;
 
+ClosedPolyline::ClosedPolyline(IWindowAPI& _windowAPI) : 
+	OpenPolyline(_windowAPI)
+{
+	
+}
+
+void ClosedPolyline::draw() const
+{
+	windowAPI->setDrawingColor(getLineColor());
+
+	if (getNumberOfPoints() < 3)
+		throw runtime_error("Une forme fermée doit avoir au moins 3 lignes.");
+
+	for (unsigned int i = 0; i < getNumberOfPoints() - 1; ++i)
+	{
+		windowAPI->drawLine(getPoint(i), getPoint(i + 1));
+	}
+	windowAPI->drawLine(getPoint(getNumberOfPoints() -1), getPoint(1));
+}
