9704493289f282150ad763382feb96cd1d89bf83
Dominique Septembre <dominique.septembre@gmail.com>
2015-03-12 13:23:08 -0400

    Shape Factory

--- Shape/shapeFactory.h
+++ Shape/shapeFactory.h
@@ -6,7 +6,13 @@ namespace ShapeLibrary
 	class	ShapeFactory
 	{
 	public:
-
-		
+		ShapeFactory(IWindowAPI &_windowAPI);
+		Shape& createOpenPolyLine();
+		Shape& createCircle(Point &_center, int _radius);
+		Shape& createRectangle(Point &_position, int _width, int _height);
+		Shape& createClosedPolyLine();
+		Shape& createPolygone();
+	private:
+		IWindowAPI *windowAPI;
 	};
 }
