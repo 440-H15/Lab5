6dc1c965527639e865a7d6513f9e193d625b0406
samueldesga <samueldesga@gmail.com>
2015-03-12 11:38:26 -0400

    Test X et Y négatif trhow exception OK
    Modification du constructeur de point

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -5,11 +5,12 @@
 using namespace ShapeLibrary;
 
 
-Point::Point( double _x, double _y ) :
-	x(_x), 
-	y(_y)
+Point::Point( double _x, double _y )
 {	
-	
+	if (_x < 0) throw invalid_argument("La coordonnée X doit être positive");
+	if (_y < 0) throw invalid_argument("La coordonnée Y doit être positive");
+	x = _x;
+	y = _y;
 }
 
 bool Point::operator == (const Point & point) const
