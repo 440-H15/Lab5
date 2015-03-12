944581c233ebe11fe8af5827beccd7930f7703aa
Dominique Septembre <dominique.septembre@gmail.com>
2015-03-12 12:51:09 -0400

    WindowRender Ok

--- AnimationRender/WindowRender.cpp
+++ AnimationRender/WindowRender.cpp
@@ -12,4 +12,37 @@ void WindowsRender::render()
 {
 	if (windowAPI->hasEvent())
 		windowAPI->getEvent();
+
+	for (Shape* shape : shapes)
+	{
+		shape->draw();
+	}
+
+	windowAPI->clearScreen();
+	windowAPI->displayScreen();
+	windowAPI->wait(1000);
+}
+
+void WindowsRender::attach(Shape& _shape)
+{
+	shapes.push_back(&_shape);
+}
+
+void WindowsRender::putOnTop(Shape& _shape)
+{
+	vector<Shape*> tempoShapes;
+
+	while (shapes.front() != &_shape)
+	{
+		tempoShapes.push_back(shapes.front());
+		shapes.pop_back();
+	}
+	shapes.pop_back();
+
+	for (int i = tempoShapes.size(); i > 0; i--)
+	{
+		shapes.push_back(tempoShapes.front());
+		tempoShapes.pop_back();
+	}
+	shapes.push_back(&_shape);
 }
