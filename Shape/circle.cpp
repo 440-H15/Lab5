e58fdd4b43fc3cf175ac1e42f81fad8142a36f79
Jeffs24 <jeffs24@live.ca>
2015-03-13 14:59:08 -0400

    circle
    Finished circle.

--- Shape/circle.cpp
+++ Shape/circle.cpp
@@ -6,6 +6,9 @@ using namespace ShapeLibrary;
 Circle::Circle(IWindowAPI& _windowAPI) :
 radius(1)
 {
+	windowAPI = &_windowAPI;
+	lineColor = Color::WHITE;
+	fillColor = Color::INVISIBLE;
 }
 
 Circle::~Circle()
@@ -14,14 +17,21 @@ Circle::~Circle()
 
 void Circle::setCenter(Point _point)
 {
+	points.pop_back();
+	points.push_back(_point);
 }
 
 void Circle::setRadius(int _radius)
 {
+	if (_radius < 0) throw invalid_argument("Le rayon doit être plus grand que 0");
 	radius = _radius;
 }
 
 void Circle::draw()
 {
-
+	if (points.size() == 0) throw runtime_error("Le point du centre est obligatoire");
+	windowAPI->setDrawingColor(lineColor);
+	windowAPI->drawCircle(points[0], radius);
+	windowAPI->setDrawingColor(fillColor);
+	windowAPI->fillCircle(points[0], radius);
 }
