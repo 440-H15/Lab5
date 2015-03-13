9994b5d30d168ae4ba4efa8884458daef01258b4
samueldesga <samueldesga@gmail.com>
2015-03-12 21:11:10 -0400

    Création de la classe circle et tout test OK
    Codé: constructeur, setCenter, setRadius, draw, setFillColor,
    setLineColor

--- Shape/circle.cpp
+++ Shape/circle.cpp
@@ -2,3 +2,38 @@
 
 using namespace ShapeLibrary;
 
+Circle::Circle(IWindowAPI& _windowAPI)
+{
+	lineColor = Color::BLUE;
+	point = nullptr;
+	radius = 0;
+	windowAPI = &_windowAPI;
+}
+
+void Circle::setCenter(Point& _point)
+{
+	point = &_point;
+}
+
+void Circle::setRadius(int _radius)
+{
+	if (_radius < 0) throw invalid_argument("Le rayon doit être positif");
+	radius = _radius;
+}
+
+void Circle::draw()
+{
+	if (point == nullptr) throw runtime_error("Le cercle n'a pas de point centrale");
+	this->windowAPI->setDrawingColor(lineColor);
+	this->windowAPI->drawCircle(*point, radius);
+}
+
+void Circle::setFillColor(Color _color)
+{
+	this->windowAPI->fillCircle(*point, radius);
+}
+
+void Circle::setLineColor(Color _color)
+{
+	lineColor = _color;
+}
--- Shape/circle.h
+++ Shape/circle.h
@@ -5,6 +5,17 @@ namespace ShapeLibrary
 {
 	class Circle
 	{
-	
+	public:
+		Circle(IWindowAPI& _windowAPI);
+		void setCenter(Point& point);
+		void setRadius(int _radius);
+		void draw();
+		void setFillColor(Color _color);
+		void setLineColor(Color _color);
+	private:
+		IWindowAPI* windowAPI;
+		Point* point;
+		Color lineColor;
+		int radius;
 	};
 }
